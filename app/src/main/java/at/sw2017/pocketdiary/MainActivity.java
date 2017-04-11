21f363578c3044cef7dddc34ecf918db775f8efb
Nicole Schuster <n.schuster@student.tugraz.at>
2017-04-11 15:19:06 +0200

    [NS,MS] create database

--- app/src/main/java/at/sw2017/pocketdiary/MainActivity.java
+++ app/src/main/java/at/sw2017/pocketdiary/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper pd_db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pd_db = new DatabaseHelper(this);
     }
 }
